bf1f2de8ef63f83d419802a2d4d699ee9d2f0df9
e1401165 <e1401165@WA3008-08.ad.puv.fi>
2017-04-13 11:35:51 +0300

    asd

--- src/main/java/myapp/Application.java
+++ src/main/java/myapp/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 //start the app before running the Maven tests
 @SpringBootApplication
-@Import (SwaggerConfig.class) //for Swagger documentation - Swagger home http://localhost:8080/swagger-ui.html
+@Import (SwaggerConfig.class) //for Swagger documentation - Swagger home http://localhost:8080/swagger-ui.htm
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
